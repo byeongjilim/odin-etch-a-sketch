2d647e4df13e2814a235a9ab71ec0ddbf9fb585b
Byeongji Lim <byeongjilim@gmail.com>
2022-02-23 19:05:15 -0500

    Add UI touches

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Method for creating the etch board.
-const createBoard = () => {
-    for (let i = 0; i < 16; i++) {
+const createBoard = (row) => {
+    let board = document.getElementsByTagName('main')[0];
+    let width = board.clientWidth / row;
+
+    for (let i = 0; i < row; i++) {
         let etchPixelRow = document.createElement('div');
         etchPixelRow.classList.add('etch-pixel-row')
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < row; j++) {
             let etchPixel = document.createElement('div');
-            etchPixel.classList.add('etch-pixel');
+            etchPixel.style.width = width + 'px';
+            etchPixel.style.height = width + 'px';
             etchPixel.addEventListener('mouseover', () => changeColor(etchPixel));
             etchPixelRow.appendChild(etchPixel);
         }
@@ -19,16 +23,22 @@ const changeColor = (etchPixel) => {
 }
 
 // Reset board.
-const reset = () => {
+const reset = (row) => {
     let board = document.getElementsByTagName('main')[0];
 
     while (board.firstChild) {
         board.removeChild(board.firstChild);
     }
 
-    createBoard();
+    createBoard(row);
+}
+
+const setNew = () => {
+    let row = document.getElementById('row').value;
+
+    reset(row);
 }
 
 window.onload = () => {
-    createBoard();
+    setNew();
 }
